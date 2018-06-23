c7480efb32b8924c597485c219fc344708f821f8
Valerie Moore <periwinkle17@outlook.com>
2018-06-23 22:27:35 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Song
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    self.name = name
+    @@artists << self.artist = artist
+    @@genres << self.genre = genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def Song.artists
+    @@artists.uniq
+  end
+  
+  def Song.genres
+    @@genres.uniq
+  end
+  
+  def Song.genre_count
+    
+  end
+  
+end
